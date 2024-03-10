bd0ca83116a8488d905a0d9041e780b669b31474
David J. Fink <david@darkroastcreative.co>
2024-03-10 12:49:40 -0600

    Added additional configuration variable to envconfig.py to allow the user to target specific sample types.

--- envconfig.py
+++ envconfig.py
@@ -6,3 +6,7 @@ DATA_SAMPLE_SIZE: int = 1000
 # File path to "export.xml" from the targeted (uncompressed) Apple Health data
 # export.
 HEALTH_DATA_SOURCE_PATH: str = r'/home/david/Sync/apple_health_export/export.xml'
+
+# The desired types of health data to include. If this list is empty, all
+# available sample types will be included.
+TARGETED_SAMPLE_TYPES: list = ['HKCategoryTypeIdentifierSleepAnalysis', 'HKQuantityTypeIdentifierOxygenSaturation']
