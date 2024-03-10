4ba2147cc82e4cff0e20ec225b1284e73c26c76c
David J. Fink <david@darkroastcreative.co>
2024-03-10 12:41:49 -0600

    Added comments to envconfig.py to explain the configuration variables and their use.

--- envconfig.py
+++ envconfig.py
@@ -1,4 +1,8 @@
-# TODO: Add comments to explain what each item is/means and how to use.
-
-HEALTH_DATA_SOURCE_PATH: str = r'Apple Health 20240205T2328.xml'
+# The desired number of records to be included in the "sample" file exported
+# by the "Generate Clean Data Files from Source" script. If no sample sample
+# file is desired, set this value to 0 (or less than 0).
 DATA_SAMPLE_SIZE: int = 1000
+
+# File path to "export.xml" from the targeted (uncompressed) Apple Health data
+# export.
+HEALTH_DATA_SOURCE_PATH: str = r'/home/david/Sync/apple_health_export/export.xml'
